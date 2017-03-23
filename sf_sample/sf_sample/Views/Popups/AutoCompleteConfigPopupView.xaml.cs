29a5122addf7fce0c400975781a4bca705a0b718
adaml984 <adam.lamarz@outlook.com>
2017-03-23 11:59:16 +0100

    test popup

---
+++ sf_sample/sf_sample/Views/Popups/AutoCompleteConfigPopupView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace sf_sample.Views.Popups
+{
+    public partial class AutoCompleteConfigPopupView
+    {
+        public const string NavigationId = "AutoCompleteConfigPopupView";
+
+        public AutoCompleteConfigPopupView()
+        {
+            InitializeComponent();
+        }
+    }
+}
