a299841cc3ce47125e7f19b3be7f54842235f602
adaml984 <adam.lamarz@outlook.com>
2017-03-24 15:25:09 +0100

    charts

--- sf_sample/sf_sample/ViewModels/BarChartViewModel.cs
+++ sf_sample/sf_sample/ViewModels/BarChartViewModel.cs
@@ -1,9 +1,18 @@
 ﻿using Prism.Navigation;
+using sf_sample.Commands;
+using Xamarin.Forms;
 
 namespace sf_sample.ViewModels
 {
     public class BarChartViewModel : BaseNavigableViewModel
     {
-        public BarChartViewModel(INavigationService navigationService) : base(navigationService) { }
+        public BarChartViewModel(INavigationService navigationService) : base(navigationService) { CreateCommands(); }
+
+        private void CreateCommands()
+        {
+            OpenConfigurationCommand = new CustomCommand(
+                    async () => { })
+                {Image = ImageSource.FromFile("settings.png")};
+        }
     }
 }
--- sf_sample/sf_sample/ViewModels/PieChartViewModel.cs
+++ sf_sample/sf_sample/ViewModels/PieChartViewModel.cs
@@ -1,9 +1,18 @@
 ﻿using Prism.Navigation;
+using sf_sample.Commands;
+using Xamarin.Forms;
 
 namespace sf_sample.ViewModels
 {
     public class PieChartViewModel : BaseNavigableViewModel
     {
-        public PieChartViewModel(INavigationService navigationService) : base(navigationService) { }
+        public PieChartViewModel(INavigationService navigationService) : base(navigationService) { CreateCommands(); }
+
+        private void CreateCommands()
+        {
+            OpenConfigurationCommand = new CustomCommand(
+                    async () => { })
+                {Image = ImageSource.FromFile("settings.png")};
+        }
     }
 }
