f905ec54f0dddde969facd732e9dbed7886b557e
adaml984 <adam.lamarz@outlook.com>
2017-03-24 15:14:28 +0100

    added missing file

---
+++ sf_sample/sf_sample/Helpers/TypeHelpers.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace sf_sample.Helpers
+{
+    public static class TypeHelpers
+    {
+        public static string GetNavigationId(this Type @type) { return @type.FullName; }
+    }
+}
