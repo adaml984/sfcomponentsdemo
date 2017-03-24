fd35bf682202ff57f69977e9715ad0d5c6265e62
adaml984 <adam.lamarz@outlook.com>
2017-03-24 15:58:22 +0100

    test data for barchart

--- sf_sample/sf_sample/ViewModels/BarChartViewModel.cs
+++ sf_sample/sf_sample/ViewModels/BarChartViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Navigation;
+﻿using System.Collections.ObjectModel;
+using Prism.Navigation;
 using sf_sample.Commands;
 using Xamarin.Forms;
 
@@ -6,7 +7,49 @@ namespace sf_sample.ViewModels
 {
     public class BarChartViewModel : BaseNavigableViewModel
     {
-        public BarChartViewModel(INavigationService navigationService) : base(navigationService) { CreateCommands(); }
+        private ObservableCollection<BarChartData> _data;
+
+        public BarChartViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Data = new ObservableCollection<BarChartData>()
+            {
+                new BarChartData {Name = "David", Height = 180},
+                new BarChartData {Name = "Michael", Height = 170},
+                new BarChartData {Name = "Steve", Height = 160},
+                new BarChartData {Name = "Joel", Height = 182},
+                new BarChartData {Name = "David", Height = 180},
+                new BarChartData {Name = "Michael", Height = 170},
+                new BarChartData {Name = "Steve", Height = 160},
+                new BarChartData {Name = "Joel", Height = 182},
+                new BarChartData {Name = "David", Height = 180},
+                new BarChartData {Name = "Michael", Height = 170},
+                new BarChartData {Name = "Steve", Height = 160},
+                new BarChartData {Name = "Joel", Height = 182},
+                new BarChartData {Name = "David", Height = 180},
+                new BarChartData {Name = "Michael", Height = 170},
+                new BarChartData {Name = "Steve", Height = 160},
+                new BarChartData {Name = "Joel", Height = 182},
+                new BarChartData {Name = "David", Height = 180},
+                new BarChartData {Name = "Michael", Height = 170},
+                new BarChartData {Name = "Steve", Height = 160},
+                new BarChartData {Name = "Joel", Height = 182},
+                new BarChartData {Name = "David", Height = 180},
+                new BarChartData {Name = "Michael", Height = 170},
+                new BarChartData {Name = "Steve", Height = 160},
+                new BarChartData {Name = "Joel", Height = 182},
+                new BarChartData {Name = "David", Height = 180},
+                new BarChartData {Name = "Michael", Height = 170},
+                new BarChartData {Name = "Steve", Height = 160},
+                new BarChartData {Name = "Joel", Height = 182},
+                new BarChartData {Name = "David", Height = 180},
+                new BarChartData {Name = "Michael", Height = 170},
+                new BarChartData {Name = "Steve", Height = 160},
+                new BarChartData {Name = "Joel", Height = 182},
+            };
+            CreateCommands();
+        }
+
+        public ObservableCollection<BarChartData> Data { get { return _data; } set { SetProperty(ref _data, value); } }
 
         private void CreateCommands()
         {
---
+++ sf_sample/sf_sample/ViewModels/Data/BarChartData.cs
@@ -0,0 +1,9 @@
+﻿namespace sf_sample.ViewModels
+{
+    public class BarChartData : BaseViewModel
+    {
+        public string Name { get; set; }
+
+        public double Height { get; set; }
+    }
+}
